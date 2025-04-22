99cf7d0e72b7924d94480b2fb06bbb52af2ac417
Tracy Ramirez <tracy.ramirez92@gmail.com>
2025-04-22 12:13:53 -0600

    Devix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+//Se definen las rutas para navegación
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./components/home/home.component') },
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component'),
+  },
+  {
+    path: 'services',
+    loadComponent: () => import('./components/services/services.component'),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./components/about/about.component'),
+  },
+  {
+    path: 'contact',
+    loadComponent: () => import('./components/contact/contact.component'),
+  },
 
-export const routes: Routes = [];
+  { path: '**', redirectTo: '', pathMatch: 'full' }, // Aquí se redirecciona a la raíz si la ruta no se encuentra
+];
